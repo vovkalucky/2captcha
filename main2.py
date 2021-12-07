311b2a77bc060dc84dbd9806fc4de61b92b5f0d1
= <vladimir.inves@gmail.com>
2021-12-07 20:58:38 +0300

    add second variant

--- main2.py
+++ main2.py
@@ -11,23 +11,25 @@ import requests, time
 from global_var import API
 from twocaptcha import TwoCaptcha
 
-solver = TwoCaptcha(API)
-data_sitekey = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
-page_url ='https://www.google.com/recaptcha/api2/demo'
+def solver():
+    solver = TwoCaptcha(API)
+    data_sitekey = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
+    page_url ='https://www.google.com/recaptcha/api2/demo'
 
-try:
-    result = solver.hcaptcha(
-        sitekey=data_sitekey,
-        url=page_url,
-    )
+    try:
+        result = solver.hcaptcha(
+            sitekey=data_sitekey,
+            url=page_url,
+        )
 
-except Exception as e:
-    sys.exit(e)
+    except Exception as e:
+        sys.exit(e)
 
-else:
-    sys.exit('solved: ' + str(result))
+    else:
+        sys.exit('solved: ' + str(result))
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    solver()
 
 
 
